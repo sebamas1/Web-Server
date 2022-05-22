bd7075f425fc5edeb984c92929470d7b29aad32e
Sebastian <sebamas1@hotmail.com>
2022-05-22 17:41:35 -0300

    feat: esta funcionando el servicio de contador y listado de usuarios.

--- bin/main.c
+++ bin/main.c
@@ -5,24 +5,127 @@
  * gcc -o test test.c -lulfius
  */
 #include <stdio.h>
+#include <ctype.h>
 #include <ulfius.h>
 #include <string.h>
 #include <jansson.h>
-//#include "json.h"
+
+#include <errno.h>
+#include <sys/types.h>
+#include <pwd.h>
 
 #define PORT 8080
 
-/**
- * Callback function for the web application on /helloworld url call
- */
+// esta funcion imprime el nombre de todos los usuarios presentes en el sistema usando getpwent a
+// partir del usuario sebastian
+void obtener_usuarios(char **usuarios, int cantidad_usuarios)
+{
+    struct passwd *pw;
+    int i = 0;
+    setpwent();
+    while (strcmp((pw = getpwent())->pw_name, "sebastian"))
+    {
+    }
+    do
+    {
+       // usuarios[i] = pw->pw_name;
+        usuarios[i] = malloc(strlen(pw->pw_name) + 1);
+        sprintf(usuarios[i], "%s", pw->pw_name);
+        i++;
+
+    } while ((pw = getpwent()) != NULL && i < cantidad_usuarios);
+    endpwent();
+}
+
+int get_cantidad_usuarios(void)
+{
+    struct passwd *pw;
+    int i = 0;
+    setpwent();
+    while (strcmp((pw = getpwent())->pw_name, "sebastian"))
+    {
+    }
+    do
+    {
+        i++;
+    } while ((pw = getpwent()) != NULL);
+    endpwent();
+    return i;
+}
+
+int getid(char *name)
+{
+    struct passwd *p;
+    if ((p = getpwnam(name)) == NULL)
+    {
+        perror(name);
+        return EXIT_FAILURE;
+    }
+    return (int)p->pw_uid;
+}
+
+// devuelve un string con año-mes-dia hora:minuto:segundo
+char *get_time_string()
+{
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    char *time_string = malloc(sizeof(char) * 20);
+    sprintf(time_string, "%d-%02d-%02d %02d:%02d:%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    return time_string;
+}
+
+int validar_password(const char *password)
+{
+    size_t largo_password;
+    largo_password = strlen(password);
+    if (largo_password < 5)
+    {
+        return -1;
+    }
+    if (largo_password > 10)
+    {
+        return -2;
+    }
+    for (size_t i = 0; i < largo_password; i++)
+    {
+        if (!isdigit(password[i]))
+        {
+            return -1;
+        }
+    }
+
+    if (strchr(password, ' ') != NULL)
+    {
+        return -1;
+    }
+    return 0;
+}
+
+int validar_usuario(const char *usuario)
+{
+    size_t largo_usuario;
+    char *caracteres_prohibidos = "\\/:*?\"<>|&;";
+    largo_usuario = strlen(usuario);
+    for (size_t i = 0; i < largo_usuario; i++)
+    {
+        if (strchr(caracteres_prohibidos, usuario[i]) != NULL)
+        {
+            return -1;
+        }
+    }
+    return 0;
+}
 
 size_t i = 0;
 
-int parse_json(json_t *root, const char **user_name, const char **user_pass){
-    if(root){
+int parse_json(json_t *root, const char **user_name, const char **user_pass)
+{
+    if (root)
+    {
         json_t *user_name_json = json_object_get(root, "user_name");
         json_t *user_pass_json = json_object_get(root, "password");
-        if(user_name_json && user_pass_json){
+        if (user_name_json && user_pass_json)
+        {
             *user_name = json_string_value(user_name_json);
             *user_pass = json_string_value(user_pass_json);
             return 0;
@@ -30,52 +133,44 @@ int parse_json(json_t *root, const char **user_name, const char **user_pass){
     }
     return -1;
 }
-//funcion que crea un body response en formato json de la forma:
+// funcion que crea un body response en formato json de la forma:
 //{
-//      "id": 142,
-//      "username": "myuser",
-//      "created_at": "2019-06-22 02:19:59"
-// }
-json_t* create_json_response(int id, const char *username, const char *created_at){
+//       "id": 142,
+//       "username": "myuser",
+//       "created_at": "2019-06-22 02:19:59"
+//  }
+json_t *create_json_respose_useradd(int id, const char *username, const char *created_at)
+{
     json_t *response = json_object();
     json_object_set_new(response, "id", json_integer(id));
     json_object_set_new(response, "username", json_string(username));
     json_object_set_new(response, "created_at", json_string(created_at));
     return response;
 }
-
-
-size_t contar_cifras_numero(size_t numero_a_contar)
+//Esta funcion crea un json array donde cada elemento lista username y user_id
+json_t *create_json_array_usuarios(char **usuarios, int cantidad_usuarios)
 {
-    size_t contador = 1;
-    size_t num = numero_a_contar;
-
-    while (num / 10 > 0)
+    json_t *array = json_array();
+    for (int i = 0; i < cantidad_usuarios; i++)
     {
-        num = num / 10;
-        contador++;
+        json_t *element = json_object();
+        json_object_set_new(element, "username", json_string(usuarios[i]));
+        json_object_set_new(element, "user_id", json_integer(getid(usuarios[i])));
+        json_array_append_new(array, element);
     }
-    return contador;
+    return array;
 }
-
 
 int callback_usuarios_creados(__attribute__((unused)) const struct _u_request *request, struct _u_response *response, __attribute__((unused)) void *user_data)
 {
-    char *inicio_respuesta = "Numero de usuarios nuevos creados: ";
-    size_t largo_respuesta = strlen(inicio_respuesta);
-    size_t cifras = contar_cifras_numero(i);
-    char aux[cifras];
-    memset(aux, '\0', cifras);
+    int cantidad_usuarios = get_cantidad_usuarios();
+    char * usuarios[cantidad_usuarios];
 
-    char respuesta[largo_respuesta + cifras];
-    memset(respuesta, '\0', largo_respuesta + cifras);
-    strcat(respuesta, inicio_respuesta);
+    obtener_usuarios(usuarios, cantidad_usuarios);
 
-    sprintf(aux, "%lu", i);
-    strcat(respuesta, aux);
-    strcat(respuesta, "\n");
+    json_t *response_json = create_json_array_usuarios(usuarios, cantidad_usuarios);
 
-    ulfius_set_string_body_response(response, 400, respuesta);
+    ulfius_set_json_body_response(response, 200, response_json);
 
     return U_CALLBACK_CONTINUE;
 }
@@ -92,12 +187,24 @@ int callback_useradd(__attribute__((unused)) const struct _u_request *request, s
         return U_CALLBACK_CONTINUE;
     }
 
-    if(parse_json(mensaje_json, &user_name, &user_pass)){
+    if (parse_json(mensaje_json, &user_name, &user_pass))
+    {
         ulfius_set_string_body_response(response, 400, "Se debe ingresar usuario y contraseña\n");
         return U_CALLBACK_CONTINUE;
     }
+    if (validar_usuario(user_name))
+    {
+        ulfius_set_string_body_response(response, 400, "El nombre de usuario no puede contener caracteres especiales.\n");
+        return U_CALLBACK_CONTINUE;
+    }
+    if (validar_password(user_pass))
+    {
+        ulfius_set_string_body_response(response, 400, "La contraseña debe tener entre 5 y 10 numeros y debe estar compuesta unicamente por caracteres numericos\n");
+        return U_CALLBACK_CONTINUE;
+    }
+    char *created_at = get_time_string();
 
-    ulfius_set_json_body_response(response, 200, create_json_response((int) i, user_name, "2019-06-22 02:19:59"));
+    ulfius_set_json_body_response(response, 200, create_json_respose_useradd(getid("sebastian"), user_name, created_at));
     i++;
     return U_CALLBACK_CONTINUE;
 }
@@ -114,8 +221,8 @@ int main(void)
     }
 
     // Endpoint list declaration
-    ulfius_add_endpoint_by_val(&instance, "GET", "/contadorusuarios", NULL, 1, &callback_usuarios_creados, NULL);
-    ulfius_add_endpoint_by_val(&instance, "POST", "/useradd", NULL, 0, &callback_useradd, NULL);
+    ulfius_add_endpoint_by_val(&instance, "GET", "/api/users", NULL, 1, &callback_usuarios_creados, NULL);
+    ulfius_add_endpoint_by_val(&instance, "POST", "/api/users", NULL, 0, &callback_useradd, NULL);
 
     // Start the framework
     if (ulfius_start_framework(&instance) == U_OK)
